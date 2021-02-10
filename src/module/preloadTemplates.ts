e7c0be7b799785fcab43b2b82b86648237e3135c
Jeremiah Verba <jeremiah.verba@gmail.com>
2021-02-09 19:50:40 -0500

    initial project creation
    https://gitlab.com/foundry-projects/foundry-pc/create-foundry-project/-/wikis/home

---
+++ src/module/preloadTemplates.ts
@@ -0,0 +1,7 @@
+export const preloadTemplates = async function() {
+	const templatePaths = [
+		// Add paths to "modules/foundryQuickStatusModule/templates"
+	];
+
+	return loadTemplates(templatePaths);
+}
